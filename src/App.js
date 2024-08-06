90f54834de3dd3038dd940282111841aa783e710
phuc <nguyenphuc200x@gmail.com>
2024-08-06 22:08:48 +0700

    build: reset css, header, nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './Reset.css';
+import logo from './assets/logo_messenger.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className='heading'>
+        <div className='content'>
+          <nav className='nav'>
+            <div className='logo-Messenger'>
+            <img src={logo} alt="logo" className='logo'/>
+            </div>
+            <div className='nav-father'>
+              <ul className='nav-bar'>
+                <li className='nav-item'><a href='#!'>Tính năng</a></li>
+                <li className='nav-item'><a href='#!'>Ứng dụng dành cho máy tính</a></li>
+                <li className='nav-item'><a href='#!'>Quyền riêng tư & an toàn</a></li>
+                <li className='nav-item'><a href='#!'>Dành cho nhà phát triển</a></li>
+              </ul>
+            </div>
+          </nav>
+          
+        </div>
       </header>
-    </div>
+      
+    </>
   );
 }
 
