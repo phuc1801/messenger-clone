6b350e4e3da16784b85b71d849b8e4a5ec7a3cd9
phuc <nguyenphuc200x@gmail.com>
2024-08-07 20:23:26 +0700

    build: components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import './App.css';
 import './Reset.css';
-import logo from './assets/logo_messenger.png';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <>
-      <header className='heading'>
-        <div className='content'>
-          <nav className='nav'>
-            <div className='logo-Messenger'>
-            <img src={logo} alt="logo" className='logo'/>
-            </div>
-            <div className='nav-father'>
-              <ul className='nav-bar'>
-                <li className='nav-item'><a href='#!'>Tính năng</a></li>
-                <li className='nav-item'><a href='#!'>Ứng dụng dành cho máy tính</a></li>
-                <li className='nav-item'><a href='#!'>Quyền riêng tư & an toàn</a></li>
-                <li className='nav-item'><a href='#!'>Dành cho nhà phát triển</a></li>
-              </ul>
-            </div>
-          </nav>
-          
-        </div>
-      </header>
-      
-    </>
+    <Header/>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import logo from './images/logo_messenger.png';
+
+function Header(){
+    return (
+        <>
+            <header className='heading'>
+                <div className='content'>
+                <nav className='nav'>
+                    <div className='logo-Messenger'>
+                    <img src={logo} alt="logo" className='logo'/>
+                    </div>
+                    <div className='nav-father'>
+                    <ul className='nav-bar'>
+                        <li className='nav-item'><a href='#!'>Tính năng</a></li>
+                        <li className='nav-item'><a href='#!'>Ứng dụng dành cho máy tính</a></li>
+                        <li className='nav-item'><a href='#!'>Quyền riêng tư & an toàn</a></li>
+                        <li className='nav-item'><a href='#!'>Dành cho nhà phát triển</a></li>
+                    </ul>
+                    </div>
+                </nav>
+                
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header;
